d0f8764eb96f6af87f1c12decbbe312bcd1db308
blainicus <blainicus@gmail.com>
2021-08-01 01:51:36 -0500

    stuff

--- StS-DefaultModBase-master/warlockMod/src/main/java/warlockMod/cards/CurseOfAgony.java
+++ StS-DefaultModBase-master/warlockMod/src/main/java/warlockMod/cards/CurseOfAgony.java
@@ -27,7 +27,7 @@ public class CurseOfAgony extends CustomCard{
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheWarlock.Enums.COLOR_GRAY;
