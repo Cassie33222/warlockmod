13ee024ba8094a725a9123bc97c8a84af3946186
blainicus <blainicus@gmail.com>
2021-08-06 02:12:13 -0500

    stuff

--- StS-DefaultModBase-master/warlockMod/src/main/java/warlockMod/cards/SoulstoneAdd.java
+++ StS-DefaultModBase-master/warlockMod/src/main/java/warlockMod/cards/SoulstoneAdd.java
@@ -2,19 +2,22 @@ package warlockMod.cards;
 
 import basemod.AutoAdd;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.cards.blue.BallLightning;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.orbs.Lightning;
 import warlockMod.WarlockMod;
 import warlockMod.characters.TheWarlock;
-import warlockMod.powers.CommonPower;
+import warlockMod.orbs.SoulShard;
 import warlockMod.powers.Spellpower;
 
-public class SpellpowerAdd extends AbstractDynamicCard {
+public class SoulstoneAdd extends AbstractDynamicCard {
 
-    public static final String ID = WarlockMod.makeID(SpellpowerAdd.class.getSimpleName());
+    public static final String ID = WarlockMod.makeID(SoulstoneAdd.class.getSimpleName());
     public static final String IMG = WarlockMod.makeCardPath("Spellpower.png");
 
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
@@ -29,22 +32,25 @@ public class SpellpowerAdd extends AbstractDynamicCard {
     private static final int UPGRADE_MAGIC = 1;
 
 
-    public SpellpowerAdd() {
+    public SoulstoneAdd() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         magicNumber = baseMagicNumber = MAGIC;
-        this.isLocked=true;
-    }
-    @Override
-    public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p,
-                new Spellpower(p, p, magicNumber), magicNumber));
+
     }
 
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //channel soul shards 1 / upgraded 2
+        for(int i=0; i<magicNumber; i++) {
+            AbstractDungeon.player.channelOrb(new SoulShard());
+        }
+    }
     @Override
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
             upgradeMagicNumber(UPGRADE_MAGIC);
+           //rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
